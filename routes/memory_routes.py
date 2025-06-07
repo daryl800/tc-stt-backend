2fef4447bef3c1d72c707290aeba20c7e3224f2c
Daryl <ng.daryl@gmail.com>
2025-06-07 22:49:37 +0800

    removed saving createdAt to leanCloud

--- routes/memory_routes.py
+++ routes/memory_routes.py
@@ -14,7 +14,7 @@ router = APIRouter()
 @router.post("/")
 def save_memory(data: dict):
     memory = Memory()
-    memory.set("createdAt", datetime.now().isoformat())
+    # memory.set("createdAt", datetime.now().isoformat())
     memory.set("text", data.get("text", ""))
     memory.set("mainEvent", data.get("event", ""))
     memory.set("reminderDatetime", data.get("reminderDatetime", ""))
