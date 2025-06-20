a25bdcf5c3693c2f01d77523d394fb33418cfb1e
Daryl <ng.daryl@gmail.com>
2025-06-20 11:18:16 +0800

    test response time

--- main.py
+++ main.py
@@ -129,7 +129,8 @@ async def process_message(websocket: WebSocket, msg_type: str, payload: str):
             initial_tts = base64.b64encode(
                 tencent_tts("咁你要俾啲耐性我，我而家幫你搵吓你之前有冇提及過关于" + extraction.mainEvent + "嘅嘢")
             ).decode()
-            await enqueue_audio(websocket, initial_tts)
+            # await enqueue_audio(websocket, initial_tts)
+            await reply_to_FE(websocket, 'audio', initial_tts)
 
             try:
                 answer = search_past_events(extraction)  # Assume this returns a list
