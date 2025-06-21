b47511677682a844da78874055c4a3b364ac5b85
Daryl <ng.daryl@gmail.com>
2025-06-21 12:12:20 +0800

    fix import

--- config/constants.py
+++ config/constants.py
@@ -1,5 +1,5 @@
 import os
-from init import init_filler_messages
+from config.init import init_filler_messages
 
 TENCENT_SECRET_ID = os.getenv("TENCENT_SECRET_ID_CN")
 TENCENT_SECRET_KEY = os.getenv("TENCENT_SECRET_KEY_CN")
