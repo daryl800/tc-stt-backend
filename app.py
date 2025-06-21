50d4c6c29658e59f34b918c9271d3d211f24a0c2
Daryl <ng.daryl@gmail.com>
2025-06-21 12:59:44 +0800

    removed wrong import

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from fastapi.responses import JSONResponse
 from fastapi.requests import Request
 from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException as StarletteHTTPException
-from config.init import init_leancloud, init_filler_messages
+from config.init import init_leancloud
 from fastapi import WebSocket, WebSocketDisconnect
 from routes.ws_routes import router as ws_router
 
