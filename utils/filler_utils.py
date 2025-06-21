83bae6052a7af255e791f2b0004549f2482a728b
Daryl <ng.daryl@gmail.com>
2025-06-21 13:46:57 +0800

    added pause to filler

--- utils/filler_utils.py
+++ utils/filler_utils.py
@@ -3,7 +3,12 @@ import random
 from utils.text_to_speech import tencent_tts
 
 # FILLER_LIST = ["嗯", "好吖", "等陣", "噉我睇吓", "畀啲時間我"]
-FILLER_LIST = ["Okay，收到～", "好呀，聽到你讲～", "收到，明白～"]
+FILLER_LIST = [
+    "嗯……Okay，收到～",
+    "噉……好呀，聽到你讲～",
+    "唔……收到，明白～"
+]
+
 FILLER_CACHE = {}
 
 def init_filler_messages():
@@ -11,4 +16,6 @@ def init_filler_messages():
     FILLER_CACHE = {text: tencent_tts(text) for text in FILLER_LIST}
 
 def pick_random_filler():
-    return FILLER_CACHE[random.choice(FILLER_LIST)]
+    chosen_text = random.choice(FILLER_LIST)
+    print(f"Chosen filler text: {chosen_text}")
+    return FILLER_CACHE[chosen_text]
