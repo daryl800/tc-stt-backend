5dbda2fd7ea90a273571d5cff63cbb64418c4dc5
Daryl <ng.daryl@gmail.com>
2025-06-25 16:26:36 +0800

    updated filler messages again

--- utils/filler_utils.py
+++ utils/filler_utils.py
@@ -4,8 +4,8 @@ from utils.text_to_speech import tencent_tts
 
 # FILLER_LIST = ["嗯", "好吖", "等陣", "噉我睇吓", "畀啲時間我"]
 FILLER_LIST = [
-    "好的……收到～",
-    # "唔唔唔……收到～",
+    "好的～收到……",
+    # "唔唔唔～收到……",
     "好的……听到～",
     # "唔唔唔……听到～"
 ]
