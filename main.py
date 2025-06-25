4ba34139fc95a73eaf36a238c3f64136ece9efc7
Daryl <ng.daryl@gmail.com>
2025-06-25 16:30:51 +0800

    updated filler messages again 2

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ async def process_message(websocket: WebSocket, msg_type: str, payload: str):
             if is_query:
                 # Initial response (always sent first)
                 initial_tts = base64.b64encode(
-                    tencent_tts("等一阵……我揾揾～")
+                    tencent_tts("等一阵……比少少时间我揾揾～")
                 ).decode()
                 # await enqueue_audio(websocket, initial_tts)
                 await reply_to_FE(websocket, 'audio', initial_tts)
