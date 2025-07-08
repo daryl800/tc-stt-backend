261712040b212b8b7e82437ead5659fcb8c8ebee
Daryl <ng.daryl@gmail.com>
2025-07-09 01:28:44 +0800

    changed model

--- utils/llm_utils.py
+++ utils/llm_utils.py
@@ -117,8 +117,8 @@ def extract_info_withLLM(text):
 
         req = models.ChatCompletionsRequest()
         req.Messages = [{"Role": "user", "Content": prompt}]
-        req.Model = "hunyuan-standard"
-        req.Temperature = 0.7
+        req.Model = "hunyuan-turbo"
+        req.Temperature = 1
 
         resp = client.ChatCompletions(req)
         data = json.loads(resp.Choices[0].Message.Content.strip())
