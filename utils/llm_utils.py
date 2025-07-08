66fb0e1c7b328d00d3924e5a1247e69225481652
Daryl <ng.daryl@gmail.com>
2025-07-09 01:11:14 +0800

    change hunyuan-t1-latest model for reflection

--- utils/llm_utils.py
+++ utils/llm_utils.py
@@ -191,8 +191,8 @@ def generate_reflection(text: str) -> str:
 
         req = models.ChatCompletionsRequest()
         req.Messages = [{"Role": "user", "Content": prompt}]
-        req.Model = "hunyuan-standard"
-        req.Temperature = 0.7
+        req.Model = "hunyuan-t1-latest"
+        req.Temperature = 1
 
         resp = client.ChatCompletions(req)
         reflection = resp.Choices[0].Message.Content.strip()
