72e221af67ac9c72104c9d802bfd42f077114243
daryl800 <ng.daryl@gmail.com>
2025-08-06 15:27:02 +0800

    refeactored debug message

--- utils/transcription.py
+++ utils/transcription.py
@@ -1,5 +1,6 @@
 # utils/transcription.py
 
+from config.constants import TENCENT_SECRET_ID, TENCENT_SECRET_KEY
 import sys
 import os
 import uuid
@@ -18,7 +19,6 @@ import asyncio
 
 # Add parent directory of utils
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-from config.constants import TENCENT_SECRET_ID, TENCENT_SECRET_KEY
 
 # Setup credentials
 cred = credential.Credential(TENCENT_SECRET_ID, TENCENT_SECRET_KEY)
@@ -27,13 +27,15 @@ if shutil.which("ffmpeg") is None:
     raise EnvironmentError("ffmpeg is not installed or not in PATH")
 
 # Initialize Hunyuan client (singleton pattern)
+
+
 def get_asr_client():
-    return  asr_client.AsrClient(cred, "ap-hongkong")
+    return asr_client.AsrClient(cred, "ap-hongkong")
 
 
 async def webm_bytes_to_wav_path(webm_bytes: bytes) -> str:
     with tempfile.NamedTemporaryFile(delete=False, suffix=".webm") as webm_file:
-        webm_file.write(webm_bytes) #write the webm bytes into a file
+        webm_file.write(webm_bytes)  # write the webm bytes into a file
         webm_path = webm_file.name
 
     wav_path = webm_path.replace(".webm", ".wav")
@@ -56,6 +58,7 @@ async def webm_bytes_to_wav_path(webm_bytes: bytes) -> str:
 
     return wav_path
 
+
 async def transcribe_tencent(wav_path: str) -> str:
     try:
         client = get_asr_client()
@@ -76,18 +79,21 @@ async def transcribe_tencent(wav_path: str) -> str:
         req.from_json_string(json.dumps(params))
         print("[INFO] Sending transcription request to Tencent ASR...")
         resp = await asyncio.to_thread(client.SentenceRecognition, req)
-        print(f"[INFO] Transcription result: {resp.Result}")
+        print(
+            f"[INFO] Transcription result (inside Transcribe_tencent): {resp.Result}")
 
         return resp.Result
 
     except TencentCloudSDKException as e:
         return f"[Tencent ASR Error] {str(e)}"
 
+
 async def transcribe_base64_webm_to_text(audio_base64_webm: str) -> str:
     print("[INFO - transcribe_base64_webm_to_text: ] Converting webm to wav...")
     audio_bytes = base64.b64decode(audio_base64_webm)
     wav_path = await webm_bytes_to_wav_path(audio_bytes)
     result = await transcribe_tencent(wav_path)
-    print(f"[INFO - transcribe_base64_webm_to_text: ] transcribed result: {result}")
+    print(
+        f"[INFO - transcribe_base64_webm_to_text: ] transcribed result: {result}")
     os.remove(wav_path)
     return result
