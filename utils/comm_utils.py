fa6d235220994eea2fbe9a483481dd48103b9f63
daryl800 <ng.daryl@gmail.com>
2025-08-20 18:36:03 +0800

    added debug message

--- utils/comm_utils.py
+++ utils/comm_utils.py
@@ -17,6 +17,7 @@ async def enqueue_audio(websocket: WebSocket, base64_audio: str):
     global sending_task
     await audio_queue.put((websocket, base64_audio))
 
+    print(f"[INFO] enqueue_audio 。。。")
     if sending_task is None or sending_task.done():
         sending_task = asyncio.create_task(audio_sending_loop())
 
